a130d626f5a1f125ee032f0bfa4909fc5d79cbb0
host-codes <110379028+host-codes@users.noreply.github.com>
2025-05-10 13:14:42 +0530

    Add files via upload

---
+++ firebaselogin/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+	
+	 apiKey: "AIzaSyDaaytl89rdhGmBroRPpYEt55zmDjFefQ0",
+    authDomain: "quizwiz-91c47.firebaseapp.com",
+    projectId: "quizwiz-91c47",
+    storageBucket: "quizwiz-91c47.firebasestorage.app",
+    messagingSenderId: "318422165023",
+    appId: "1:318422165023:web:2966847b71e8777c07172a",
+    measurementId: "G-733PML4EFH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
